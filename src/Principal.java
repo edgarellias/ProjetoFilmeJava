446a24bfa9e39019a06bc6ff4b0612ed374b95ff
Edgar Elias <edgar_jk3@hotmail.com>
2023-05-11 18:57:48 -0300

    ProjetoFilmeJava

--- src/Principal.java
+++ src/Principal.java
@@ -1,16 +1,17 @@
+import ScreenMatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "Velozes e furiosos";
-        meuFilme.anoDeLancamento = 2003;
-        meuFilme.duracaoEmMinutos = 160;
+        meuFilme.setNome("Velozes e furiosos");
+        meuFilme.setAnoDeLancamento(2003);
+        meuFilme.setDuracaoEmMinutos(160);
 
         meuFilme.exibeFichaTecnica();
         meuFilme.somaDasAvaliacoes(10);
         meuFilme.somaDasAvaliacoes(3);
         meuFilme.somaDasAvaliacoes(8);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println(meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
     }
 }
